31e70284435d2c0e21de9e7a6a0286770d82303b
Mark Ryall <mark@ryall.name>
2011-05-15 14:59:37 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalpong::Application.config.secret_token = '76d166c1aeaed61d22739be66e74dfbae5ceb41474b68487ed9e4dc6a1d3f0efa42dc7fcbe56e8a1694c335ef352b813741049603c6cfdfe993d155595617eda'
